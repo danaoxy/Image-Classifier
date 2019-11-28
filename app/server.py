acb82b96603a135866e4aada59cd51b34f05ba7c
Articwolfles <58208257+Articwolfles@users.noreply.github.com>
2019-11-28 09:41:22 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,11 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    if prediction == 'trash':
+        return JSONResponse({'It is ' + str(prediction)})   
+    else:
+        return JSONResponse({'It is ' + str(prediction) + ', which is recyclable!'})
+    
 
 
 if __name__ == '__main__':
